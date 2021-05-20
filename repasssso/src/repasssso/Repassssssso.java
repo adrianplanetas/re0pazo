6ae427eaa67852d3f8030a35ff92c4c27c94fad5
adrianplanetas <dw1acovid@gmail.com>
2021-05-20 19:03:45 +0100

    inicio

---
+++ repasssso/src/repasssso/Repassssssso.java
@@ -0,0 +1,20 @@
+package repasssso;
+
+
+public class Repassssssso {
+	private Object MasterMind;
+	public boolean visualizarSalida(boolean acertada, int i, String newParam) {
+	System.out.println ();
+	if (acertada) {
+		System.out.println (i + (i-1) +" intentos en la partida nº " +MasterMind+ i);
+	} else {
+		System.out.println (i + i);
+	}
+	return acertada;
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
